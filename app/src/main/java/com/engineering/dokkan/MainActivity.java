16c3da69d8e102d11225f45959f22b735bca514b
salma <salmayassin.1997@gmail.com>
2020-03-05 20:24:22 +0200

    CircularImage , cardView for options & signout

--- app/src/main/java/com/engineering/dokkan/MainActivity.java
+++ app/src/main/java/com/engineering/dokkan/MainActivity.java
@@ -3,6 +3,11 @@ package com.engineering.dokkan;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final ArrayList<String> list = new ArrayList<String>();
+        list.add("My Orders");
+        list.add("Messages");
+        list.add("Reviews");
+        list.add("Help");
+        list.add("Languages");
+        list.add("My Address Book");
+
+        OptionAdapter adapter = new OptionAdapter(this, list);
+
+        ListView listView = (ListView) findViewById(R.id.list);
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+
+
+            }
+        });
+
     }
+
 }
