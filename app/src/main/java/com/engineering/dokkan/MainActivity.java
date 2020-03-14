1e9f13795dc2bb512e3fb4e357e72e22fcd1c445
asmaaelqady <as2161996>
2020-03-14 17:06:06 +0200

    intial commit1

--- app/src/main/java/com/engineering/dokkan/MainActivity.java
+++ app/src/main/java/com/engineering/dokkan/MainActivity.java
@@ -1,14 +1,18 @@
 package com.engineering.dokkan;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
