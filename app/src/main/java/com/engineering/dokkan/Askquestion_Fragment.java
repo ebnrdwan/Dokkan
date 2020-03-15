173295c560f3fd29133fbd7228367b8ce2a9e3b7
Huda55 <hudarabie55@gmail.com>
2020-03-16 01:04:44 +0200

    Change Activity to Fragment

---
+++ app/src/main/java/com/engineering/dokkan/Askquestion_Fragment.java
@@ -0,0 +1,31 @@
+package com.engineering.dokkan;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class Askquestion_Fragment extends Fragment {
+
+
+    public Askquestion_Fragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_askquestion_frag, container, false);
+    }
+
+}
--- app/src/main/java/com/engineering/dokkan/MainActivity.java
+++ app/src/main/java/com/engineering/dokkan/MainActivity.java
@@ -1,6 +1,8 @@
 package com.engineering.dokkan;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
@@ -10,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        Askquestion_Fragment Fragment = new Askquestion_Fragment();
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction ft = fragmentManager.beginTransaction();
+        ft.replace(R.id.container, Fragment);
+        ft.commit();
+        ft.addToBackStack(null);
+
     }
 }
+
