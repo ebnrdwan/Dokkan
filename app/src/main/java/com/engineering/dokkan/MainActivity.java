b82857df0a7f634b9e51575a588a841b2ab0b4ac
asmaaelqady <as2161996>
2020-03-15 22:46:54 +0200

    adding two button in main activity

--- app/src/main/java/com/engineering/dokkan/MainActivity.java
+++ app/src/main/java/com/engineering/dokkan/MainActivity.java
@@ -9,10 +9,37 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button button_x,button_y;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button_x=findViewById(R.id.button);
+        button_y = findViewById(R.id.button1);
+        button_x.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Notification fragx = new Notification();
+                FragmentManager fm = getSupportFragmentManager();
+                FragmentTransaction ft = fm.beginTransaction();
+                ft.replace(R.id.contener,fragx);
+                ft.addToBackStack(null);
+                ft.commit();
+
+            }
+        });
+        button_y.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               No_Notifications fragy = new No_Notifications();
+                FragmentManager fm = getSupportFragmentManager();
+                FragmentTransaction ft = fm.beginTransaction();
+                ft.replace(R.id.contener,fragy);
+                ft.addToBackStack(null);
+                ft.commit();
+            }
+        });
+
 
     }
 }
