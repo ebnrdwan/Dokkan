de95fec788c67fd66568f083c7653f0f0c429775
ebnrdwan <eng.abdoehab@gmail.com>
2020-03-22 11:07:08 +0200

    resolve merge conflicts of profile and ask questions

--- app/src/main/java/com/engineering/dokkan/MainActivity.java
+++ app/src/main/java/com/engineering/dokkan/MainActivity.java
@@ -1,16 +1,10 @@
 package com.engineering.dokkan;
 
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
-import androidx.fragment.app.FragmentManager;
-
-import android.os.Bundle;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ListView;
-
-import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,23 +13,29 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+//        navigateToProfile();
+        navigateToQuestions();
+
+
+    }
+
+
+    void navigateToProfile() {
         ProfileFragment headFragment = new ProfileFragment();
         FragmentManager fragmentManager = getSupportFragmentManager();
         fragmentManager.beginTransaction()
-                .add(R.id.head_container , headFragment)
+                .add(R.id.container, headFragment)
                 .commit();
+    }
 
-        }
-
-
-
+    void navigateToQuestions() {
         Askquestion_Fragment Fragment = new Askquestion_Fragment();
         FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction ft = fragmentManager.beginTransaction();
         ft.replace(R.id.container, Fragment);
         ft.commit();
-        ft.addToBackStack(null);
-
     }
 }
+
 
