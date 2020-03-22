beed79031705fb94a7b50f225e5f994400821faf
ebnrdwan <eng.abdoehab@gmail.com>
2020-03-22 11:23:52 +0200

    fixing comments

--- app/src/main/java/com/engineering/dokkan/MainActivity.java
+++ app/src/main/java/com/engineering/dokkan/MainActivity.java
@@ -9,37 +9,55 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    Button button_x,button_y;
+    Button button_x, button_y;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        button_x=findViewById(R.id.button);
+
+        initViews();
+        setListeners();
+    }
+
+
+    public void initViews() {
+        button_x = findViewById(R.id.button);
         button_y = findViewById(R.id.button1);
+    }
+
+    public void setListeners() {
         button_x.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Notification fragx = new Notification();
-                FragmentManager fm = getSupportFragmentManager();
-                FragmentTransaction ft = fm.beginTransaction();
-                ft.replace(R.id.contener,fragx);
-                ft.addToBackStack(null);
-                ft.commit();
 
+                navigateToNotification();
             }
         });
         button_y.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-               No_Notifications fragy = new No_Notifications();
-                FragmentManager fm = getSupportFragmentManager();
-                FragmentTransaction ft = fm.beginTransaction();
-                ft.replace(R.id.contener,fragy);
-                ft.addToBackStack(null);
-                ft.commit();
+                navigateToNoNotification();
             }
         });
+    }
 
 
+    public void navigateToNoNotification() {
+        No_Notifications fragy = new No_Notifications();
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+        ft.replace(R.id.contener, fragy);
+        ft.addToBackStack(null);
+        ft.commit();
+    }
+
+    public void navigateToNotification() {
+        Notification fragx = new Notification();
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+        ft.replace(R.id.contener, fragx);
+        ft.addToBackStack(null);
+        ft.commit();
     }
 }
