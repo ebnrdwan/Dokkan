c67d626f77632fa69983746a3349f367aa365f79
FatmaEssamMohamed <Fatma123@GitHub>
2020-04-14 17:35:42 +0200

    welcome_fragment task

--- app/src/main/java/com/engineering/dokkan/view/MainActivity.java
+++ app/src/main/java/com/engineering/dokkan/view/MainActivity.java
@@ -25,7 +25,7 @@ public class MainActivity extends AppCompatActivity {
 //        navigateToProfile();
 //        navigateToNotification();
 //        navigateToChat();
-        navigateToQuestions();
+  //      navigateToQuestions();
 
 
     }
