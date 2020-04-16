8cd6f092f0c35c98cb7dc22d18d206f18dccc985
asmaaelqady <as2161996>
2020-04-16 17:21:04 +0200

    added login branch with login fregment and  add all values in res files and using  navigation componanet add button sing in login fragment to goto profileFragment

--- app/src/main/java/com/engineering/dokkan/view/login/LoginFragment.java
+++ app/src/main/java/com/engineering/dokkan/view/login/LoginFragment.java
@@ -1,62 +1,50 @@
 package com.engineering.dokkan.view.login;
 
+import android.view.View;
+import android.widget.Button;
 
-import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Button;
-
 import com.engineering.dokkan.R;
+import com.engineering.dokkan.view.base.BaseFragment;
 
-/**
- * A simple {@link Fragment} subclass.
- */
-public class LoginFragment extends Fragment {
-
+public class LoginFragment extends BaseFragment {
     private Button btn_sing;
 
     public LoginFragment() {
-        // Required empty public constructor
-    }
 
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login2, container, false);
     }
 
     @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
+    public int getLayoutId() {
+        return (R.layout.fragment_login2);
+    }
+    NavController getNavController(){
+
+        return Navigation.findNavController(getActivity() ,R.id.nav_host_fragment);
+
+    }
+
+
+
+    @Override
+    public void initializeViews(View view) {
         initialization(view);
-        setListener(btn_sing);
+        setListeners();
     }
 
-    public void initialization(View view){   btn_sing = view.findViewById(R.id.but_sing);}
-
-
-    NavController getNavController(){ return Navigation.findNavController(getActivity() ,R.id.nav_host_fragment);}
-
-
-    public void setListener(Button btn) {
-        btn.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void setListeners() {
+        btn_sing.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 getNavController().navigate(R.id.action_loginFragment_to_profileFragment);
             }
         });
-
     }
 
+    public void initialization( View view){
 
+        btn_sing = view.findViewById(R.id.but_sing);}
 }
