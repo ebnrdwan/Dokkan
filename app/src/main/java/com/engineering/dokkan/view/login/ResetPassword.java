15f756417cd31c5597b4d101ff02312c41814cfe
asmaaelqady <as2161996>
2020-06-23 16:56:50 +0200

    add resouses in its files , add afragment to reset password making its xml add acode in reset password to change password.... add in login fragment methode of sing in with email and password to allow user to sing in

--- app/src/main/java/com/engineering/dokkan/view/login/ResetPassword.java
+++ app/src/main/java/com/engineering/dokkan/view/login/ResetPassword.java
@@ -3,30 +3,83 @@ package com.engineering.dokkan.view.login;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.engineering.dokkan.R;
+import com.engineering.dokkan.view.base.BaseFragment;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class ResetPassword extends Fragment {
-
+public class ResetPassword extends BaseFragment {
+    private EditText registeredEmail;
+    private Button resetPassword_btn;
+    private FirebaseAuth firebaseAuth;
 
     public ResetPassword() {
         // Required empty public constructor
     }
 
+    @Override
+    public int getLayoutId() { return R.layout.fragment_reset_password; }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_reset_password, container, false);
+    public void initializeViews(View view) {
+        registeredEmail = view.findViewById(R.id.rest_email);
+        resetPassword_btn = view.findViewById(R.id.reset_btn);
+        firebaseAuth=FirebaseAuth.getInstance();
+
+        resetPassword_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ResetEmail();
+            }
+        });
+
     }
 
-}
+
+
+    @Override
+    public void setListeners() {
+
+
+    }
+
+    private void ResetEmail() {
+            String email = registeredEmail.getText().toString();
+            if(TextUtils.isEmpty(email)){
+
+                Toast.makeText(getActivity(), "This email is not valid.", Toast.LENGTH_SHORT).show();
+
+            }else {
+                firebaseAuth.sendPasswordResetEmail(email)
+                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                if (task.isSuccessful()){
+                                    Toast.makeText(getActivity(), " email sent successfully.", Toast.LENGTH_SHORT).show();
+
+                                }else {
+                                    String error = task.getException().getMessage();
+                                    Toast.makeText(getActivity(), error, Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+            }
+        }
+    }
+
+
